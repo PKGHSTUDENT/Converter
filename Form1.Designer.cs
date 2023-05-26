974af03d460a15c4e3b7a080a4ccf23f625f2e68
dobrodelete <kirkon3004@gmail.com>
2023-05-27 02:50:42 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromTb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toTB = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.fromBinaryRB = new System.Windows.Forms.RadioButton();
+            this.fromDecimalRB = new System.Windows.Forms.RadioButton();
+            this.fromHexademicalRB = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.toHexadecimalRB = new System.Windows.Forms.RadioButton();
+            this.toDecimalRB = new System.Windows.Forms.RadioButton();
+            this.toBinaryRB = new System.Windows.Forms.RadioButton();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fromTb
+            // 
+            this.fromTb.Location = new System.Drawing.Point(18, 123);
+            this.fromTb.Name = "fromTb";
+            this.fromTb.Size = new System.Drawing.Size(174, 20);
+            this.fromTb.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Перевести из";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Результат";
+            // 
+            // toTB
+            // 
+            this.toTB.Location = new System.Drawing.Point(216, 123);
+            this.toTB.Name = "toTB";
+            this.toTB.Size = new System.Drawing.Size(174, 20);
+            this.toTB.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.fromHexademicalRB);
+            this.groupBox1.Controls.Add(this.fromDecimalRB);
+            this.groupBox1.Controls.Add(this.fromBinaryRB);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 92);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Из";
+            // 
+            // fromBinaryRB
+            // 
+            this.fromBinaryRB.AutoSize = true;
+            this.fromBinaryRB.Checked = true;
+            this.fromBinaryRB.Location = new System.Drawing.Point(6, 16);
+            this.fromBinaryRB.Name = "fromBinaryRB";
+            this.fromBinaryRB.Size = new System.Drawing.Size(75, 17);
+            this.fromBinaryRB.TabIndex = 0;
+            this.fromBinaryRB.TabStop = true;
+            this.fromBinaryRB.Text = "Двоичная";
+            this.fromBinaryRB.UseVisualStyleBackColor = true;
+            this.fromBinaryRB.CheckedChanged += new System.EventHandler(this.fromHexademicalRB_CheckedChanged);
+            // 
+            // fromDecimalRB
+            // 
+            this.fromDecimalRB.AutoSize = true;
+            this.fromDecimalRB.Location = new System.Drawing.Point(6, 39);
+            this.fromDecimalRB.Name = "fromDecimalRB";
+            this.fromDecimalRB.Size = new System.Drawing.Size(86, 17);
+            this.fromDecimalRB.TabIndex = 1;
+            this.fromDecimalRB.Text = "Десятичная";
+            this.fromDecimalRB.UseVisualStyleBackColor = true;
+            this.fromDecimalRB.CheckedChanged += new System.EventHandler(this.fromHexademicalRB_CheckedChanged);
+            // 
+            // fromHexademicalRB
+            // 
+            this.fromHexademicalRB.AutoSize = true;
+            this.fromHexademicalRB.Location = new System.Drawing.Point(6, 62);
+            this.fromHexademicalRB.Name = "fromHexademicalRB";
+            this.fromHexademicalRB.Size = new System.Drawing.Size(122, 17);
+            this.fromHexademicalRB.TabIndex = 2;
+            this.fromHexademicalRB.Text = "Шеснадцатеричная";
+            this.fromHexademicalRB.UseVisualStyleBackColor = true;
+            this.fromHexademicalRB.CheckedChanged += new System.EventHandler(this.fromHexademicalRB_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.toHexadecimalRB);
+            this.groupBox2.Controls.Add(this.toDecimalRB);
+            this.groupBox2.Controls.Add(this.toBinaryRB);
+            this.groupBox2.Location = new System.Drawing.Point(210, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(180, 92);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "В";
+            // 
+            // toHexadecimalRB
+            // 
+            this.toHexadecimalRB.AutoSize = true;
+            this.toHexadecimalRB.Location = new System.Drawing.Point(6, 62);
+            this.toHexadecimalRB.Name = "toHexadecimalRB";
+            this.toHexadecimalRB.Size = new System.Drawing.Size(122, 17);
+            this.toHexadecimalRB.TabIndex = 2;
+            this.toHexadecimalRB.Text = "Шеснадцатеричная";
+            this.toHexadecimalRB.UseVisualStyleBackColor = true;
+            this.toHexadecimalRB.CheckedChanged += new System.EventHandler(this.fromHexademicalRB_CheckedChanged);
+            // 
+            // toDecimalRB
+            // 
+            this.toDecimalRB.AutoSize = true;
+            this.toDecimalRB.Checked = true;
+            this.toDecimalRB.Location = new System.Drawing.Point(6, 39);
+            this.toDecimalRB.Name = "toDecimalRB";
+            this.toDecimalRB.Size = new System.Drawing.Size(86, 17);
+            this.toDecimalRB.TabIndex = 1;
+            this.toDecimalRB.TabStop = true;
+            this.toDecimalRB.Text = "Десятичная";
+            this.toDecimalRB.UseVisualStyleBackColor = true;
+            this.toDecimalRB.CheckedChanged += new System.EventHandler(this.fromHexademicalRB_CheckedChanged);
+            // 
+            // toBinaryRB
+            // 
+            this.toBinaryRB.AutoSize = true;
+            this.toBinaryRB.Location = new System.Drawing.Point(6, 16);
+            this.toBinaryRB.Name = "toBinaryRB";
+            this.toBinaryRB.Size = new System.Drawing.Size(75, 17);
+            this.toBinaryRB.TabIndex = 0;
+            this.toBinaryRB.Text = "Двоичная";
+            this.toBinaryRB.UseVisualStyleBackColor = true;
+            this.toBinaryRB.CheckedChanged += new System.EventHandler(this.fromHexademicalRB_CheckedChanged);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(18, 149);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertBtn.TabIndex = 6;
+            this.convertBtn.Text = "Перевести";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 464);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.toTB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fromTb);
+            this.Name = "Form1";
+            this.Text = "Конвертер";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fromTb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox toTB;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton fromHexademicalRB;
+        private System.Windows.Forms.RadioButton fromDecimalRB;
+        private System.Windows.Forms.RadioButton fromBinaryRB;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton toHexadecimalRB;
+        private System.Windows.Forms.RadioButton toDecimalRB;
+        private System.Windows.Forms.RadioButton toBinaryRB;
+        private System.Windows.Forms.Button convertBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fromHexademicalRB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (fromBinaryRB.Checked)
+            {
+                toBinaryRB.Enabled = false;
+                toDecimalRB.Enabled = true;
+                toHexadecimalRB.Enabled = true;
+                if (toBinaryRB.Checked)
+                {
+                    toBinaryRB.Checked = false;
+                    toDecimalRB.Checked = true;
+                }
+            }
+            if (fromDecimalRB.Checked)
+            {
+                toBinaryRB.Enabled = true;
+                toDecimalRB.Enabled = false;
+                toHexadecimalRB.Enabled = true;
+                if (toDecimalRB.Checked)
+                {
+                    toBinaryRB.Checked = true;
+                    toDecimalRB.Checked = false;
+                }
+            }
+            if (fromHexademicalRB.Checked)
+            {
+                toBinaryRB.Enabled = true;
+                toDecimalRB.Enabled = true;
+                toHexadecimalRB.Enabled = false;
+                if (toHexadecimalRB.Checked)
+                {
+                    toBinaryRB.Checked = true;
+                    toHexadecimalRB.Checked = false;
+                }
+            }
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            if (fromTb.Text.Length == 0)
+            {
+                MessageBox.Show("Пожалуйста, введите значение в поле число, которое вы хотите конвертировать.");
+                return;
+            }
+            if (fromBinaryRB.Checked)
+            {
+                int binary = 0;
+                try
+                {
+                    binary = int.Parse(fromTb.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Введите в поле двоичное число.");
+                    return;
+                }
+                if (!IsBinary(binary.ToString())) {
+                    return;
+                }
+                if (toDecimalRB.Checked)
+                {
+                    toTB.Text = fromBtoD(binary).ToString();
+                } else if (toHexadecimalRB.Checked)
+                {
+                    toTB.Text = fromBtoHex(binary.ToString());
+                }
+            } else if (fromDecimalRB.Checked)
+            {
+                int dec = 0;
+                try
+                {
+                    dec = int.Parse(fromTb.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Введите в поле простое число, без слов, символов и знаков пунктуации.");
+                    return;
+                }
+                if (!isDecimal(dec.ToString()))
+                {
+                    return;
+                }
+                if (toBinaryRB.Checked)
+                {
+                    toTB.Text = fromDtoB(dec);
+                }
+                else if (toHexadecimalRB.Checked)
+                {
+                    toTB.Text = fromDtoH(dec);
+                }
+            } else if (fromHexademicalRB.Checked)
+            {
+                string hex = fromTb.Text;
+                if (!isHex(hex.ToString()))
+                {
+                    return;
+                }
+                if (toBinaryRB.Checked)
+                {
+                    toTB.Text = fromHtoB(hex);
+                }
+                else if (toDecimalRB.Checked)
+                {
+                    toTB.Text = fromHtoD(hex);
+                }
+            } else
+            {
+                return;
+            }
+        }
+
+        private int fromBtoD(int binaryNumber)
+        {
+            int decimalValue = 0;
+            int base1 = 1;
+            while (binaryNumber > 0)
+            {
+                int reminder = binaryNumber % 10;
+                binaryNumber = binaryNumber / 10;
+                decimalValue += reminder * base1;
+                base1 = base1 * 2;
+            }
+            return decimalValue;
+        }
+
+        private string fromBtoHex(string binaryNumber)
+        {
+            if (string.IsNullOrEmpty(binaryNumber))
+                return binaryNumber;
+
+            StringBuilder result = new StringBuilder(binaryNumber.Length / 8 + 1);
+
+            int mod4Len = binaryNumber.Length % 8;
+            if (mod4Len != 0)
+            {
+                binaryNumber = binaryNumber.PadLeft(((binaryNumber.Length / 8) + 1) * 8, '0');
+            }
+
+            for (int i = 0; i < binaryNumber.Length; i += 8)
+            {
+                string eightBits = binaryNumber.Substring(i, 8);
+                result.AppendFormat("{0:X2}", Convert.ToByte(eightBits, 2));
+            }
+
+            return result.ToString();
+        }
+
+        private string fromDtoB(int decimalNumber)
+        {
+            return Convert.ToString(decimalNumber, 2);
+        }
+
+        private string fromDtoH(int decimalNumber)
+        {
+            return string.Format("{0:x}", decimalNumber);
+        }
+
+        private string fromHtoB(string hex)
+        {
+            string binarystring = String.Join(String.Empty,
+                hex.Select(
+                    c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')
+                )
+            );
+            return binarystring;
+        }
+
+        private string fromHtoD(string hex)
+        {
+            int int_value = Convert.ToInt32(hex, 16);
+            return int_value.ToString();
+        }
+
+        private bool IsBinary(string binaryNumber)
+        {
+            Regex rgx = new Regex(@"^[01]+$");
+            if (!rgx.IsMatch(binaryNumber))
+            {
+                MessageBox.Show("Введите в поле двоичное число.");
+                return false;
+            }
+            return true;
+        }
+
+        private bool isDecimal(string decimalNumber)
+        {
+            Regex rgx = new Regex("^[0-9]+$");
+            if (!rgx.IsMatch(decimalNumber))
+            {
+                MessageBox.Show("Введите в поле обычное число.");
+                return false;
+            }
+            return true;
+        }
+
+        private bool isHex(string hexNumber)
+        {
+            Regex rgx = new Regex("^[A-Fa-f0-9]*$");
+            if (!rgx.IsMatch(hexNumber))
+            {
+                MessageBox.Show("Введите в поле шеснадцатеричное число.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
